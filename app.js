3ffd8fa98f4011b73597631f22bba9f98be0dc0b
Naman Jha <jhanaman786@gmail.com>
2023-12-17 16:27:48 +0530

    Add database connection

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
+//init app and middleware
 const app = express();
 
-//init and middleware
-app.listen(3000, ()=>{
-    console.log("Server started at port 3000");
+//db connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("Server started at port 3000");
+    });
+  }
+  db = getDb();
 });
 
 //routes
-app.get("/books", (req, res)=>{
-    res.json({mssg: "Welcome to BookVault"})
+app.get("/books", (req, res) => {
+  res.send("Welcome to BookVault")
 });
