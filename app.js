517829f8387c57f834946fbba02d18e261ffba63
Naman Jha <jhanaman786@gmail.com>
2023-12-17 16:28:50 +0530

    access a file using its id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("bson");
 
 //init app and middleware
 const app = express();
@@ -18,5 +19,30 @@ connectToDb((err) => {
 
 //routes
 app.get("/books", (req, res) => {
-  res.send("Welcome to BookVault")
+  let books = [];
+  db.collection("books")
+    .find()
+    .sort({ title: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "could not fetch the documents" });
+    });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then(doc => {
+        res.status(200).json(doc);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "not a valid document id" });
+  }
 });
